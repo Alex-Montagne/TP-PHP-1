b63c3d6abe755427c8dd2c43a6c614d5199221f4
alex montagne <alex.montagne2004@gmail.com>
2025-04-08 09:03:10 +0200

    boucle for update

--- boucle_for.php
+++ boucle_for.php
@@ -2,18 +2,18 @@
 
 #Exercice 1
 
-for ($i = 1; $i <= 10; $i++){
+for($i = 1; $i <= 10; $i++){
     echo $i;
 }
 
 #Exercice 2
 
-for ($i = 1, $j = 0; $i <= 100; $j += $i, $i++);
+for($i = 1, $j = 0; $i <= 100; $j += $i, $i++);
 echo $j;
 
 #Exercice 3
 
-for ($i = 1; $i <= 10; $i++){
+for($i = 1; $i <= 10; $i++){
     for ($j = 1; $j <= 10; $j++){
         echo $i." x ".$j." = ".$i*$j."\n";
     }
